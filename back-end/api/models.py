3b80021297419b475310591e37c7f6b3678472f3
Omidi-a <omidiazam325@gmail.com>
2024-03-18 15:05:41 +0330

    add Note model superuser and add some notes

--- back-end/api/models.py
+++ back-end/api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Note(models.Model):
+
+    body = models.TextField(blank=True,null=True)
+    updated = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return f'{self.body[:50]}'
