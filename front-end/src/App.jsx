0ef5b4aefb124fc50c8727afe045240f0da58c24
Omidi-a <omidiazam325@gmail.com>
2024-03-18 15:34:12 +0330

    add browser router and routes

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/header/Header";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <Header />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Header />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
