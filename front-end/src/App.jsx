e6b3404398dcec7c0d5a99964bd8695d74d63133
Omidi-a <omidiazam325@gmail.com>
2024-03-18 15:15:56 +0330

    i sass and create Header components

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,10 +1,14 @@
 import { useState } from "react";
 import "./App.css";
-
+import Header from "./components/header/Header";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <>hello world</>;
+  return (
+    <>
+      <Header></Header>
+    </>
+  );
 }
 
 export default App;
