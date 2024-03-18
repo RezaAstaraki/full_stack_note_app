a5d06dce39abb13e944bb6601ac462646086c959
RezaAstaraki <reza.astaraky@gmail.com>
2024-03-18 17:34:30 +0330

    add node list page

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/header/Header";
+import NoteLisPage from "./pages/noteListpage/NodeListPage";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
-        <Header />
+        <Route Component={NoteLisPage} path="notes" />
       </Routes>
     </BrowserRouter>
   );
---
+++ front-end/src/pages/noteListpage/NodeListPage.jsx
@@ -0,0 +1,9 @@
+import "./nodeListPage.scss";
+
+import React from "react";
+
+function NodeListPage() {
+  return <div className="nodeListPage">NodeListPage</div>;
+}
+
+export default NodeListPage;
